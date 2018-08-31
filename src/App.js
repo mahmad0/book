80f80a756537a2ded9c69997153c5b258bb96457
Ahmad CHETTIH <achettih@squad.fr>
2018-08-31 11:56:30 +0200

    We've a got a new view for desktop

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <div className="quote-block">
           <h1 className="quote">{this.state.quote}</h1>
         </div>
